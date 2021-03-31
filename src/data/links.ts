82553cce20d258fd036ba3e4277a4327544f43c9
mnao305 <mail@mnao305.com>
2021-03-31 22:43:03 +0900

    add scrapbox

--- src/data/links.ts
+++ src/data/links.ts
@@ -4,4 +4,5 @@ export const links = [
   { name: 'Blog', url: 'http://blog.mnao305.com' },
   { name: 'Amazon wish list', url: 'http://amzn.asia/aa05E8q' },
   { name: 'Instagram', url: 'https://www.instagram.com/mnao305/' },
+  { name: 'Scrapbox', url: 'https://scrapbox.io/mnao305' },
 ]
